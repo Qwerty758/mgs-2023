d72d700c017088fc955bc73315e35af183784163
Qwerty 758 <vetercura@gmail.com>
2023-06-15 16:30:50 +0000

    proving is monoid

---
+++ src/abstract/group.py
@@ -0,0 +1,49 @@
+
+def isAssaciative(q, op):
+    
+
+def isClosed(q, op):
+    return all(op(x,y) in q
+        for x in q
+        for y in q)
+
+def isMonoid (q, op):
+    raise NotImplemented
+
+
+def findIdentity(q,op):
+    for x in q:
+            if all(op(x,y) == y and op(y,x) == y  for y in q):
+                    return x
+            return false
+
+def findInverse(x, q,op):
+    e = findIdentity
+    if e is false:
+        return False
+    else:
+            fro y in q:
+                if op(y,x) == e:
+                    return y
+        #this line cannot be reached
+
+
+
+def hasInverses (q, op):
+    raise NotImplemented
+
+
+def isGroup(q, op):
+    #raise NotImplemented
+    return isMonoid(q, op) and hasInverses(q,op)
+
+q1 ={0,1,2,3,4,5,6,7}
+q2 = {1,2,3,4}
+
+def mult(a,b):
+    return a*b %7 
+
+
+
+print(isGroup(q1,mult))
+print(isGroup(q2,mult))
